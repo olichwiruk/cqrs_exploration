fe9a76e7187420a5b12852d5f2f8b43c4391e272
Marcin <olichwiruk@gmail.com>
2017-08-16 14:19:06 +0200

    implement Entity methods

--- lib/infrastructure/domain/entity.rb
+++ lib/infrastructure/domain/entity.rb
@@ -1,3 +1,23 @@
 module Entity
 
+  def applied_events
+    @applied_events ||= []
+  end
+
+  def apply_event(event)
+    event_to_store = Event.new(event_name(event),
+                               event.instance_values)
+    do_apply event
+    applied_events << event_to_store
+  end
+
+  # @api private
+  def do_apply(event)
+    method_name = "on_#{event_name(event)}"
+    method(method_name).call(event)
+  end
+
+  def event_name(event)
+    "#{event.class.name.underscore}".sub(/_event/, '')
+  end
 end
