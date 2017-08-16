4b2f5bd1a2a0ba844085489a29366dcedfc15052
Marcin <olichwiruk@gmail.com>
2017-08-16 10:41:07 +0200

    add new and index action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
+require 'infrastructure/domain/user'
+require 'infrastructure/command_bus'
+require 'infrastructure/commands/create_user_command'
+
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
+  end
+
+  def create
+    user = User.new(params[:user]['name'],
+                    params[:user]['email'])
+    command = CreateUserCommand.new(user)
+    CommandBus::send(command)
   end
 
 end
