6599038aadc08330788e3336bc1506fd91f4727f
Marcin <olichwiruk@gmail.com>
2017-08-17 15:21:48 +0200

    add superficial validation

--- lib/infrastructure/commands/create_user_command.rb
+++ lib/infrastructure/commands/create_user_command.rb
@@ -3,5 +3,27 @@ class CreateUserCommand
 
   def initialize(user)
     @user = user
+
+    if user.name.empty?
+      raise "name can't be empty"
+    end
+
+    if user.email.empty?
+      raise "email can't be empty"
+    end
+
+    if !is_a_valid_email?(user.email)
+      raise "invalid email"
+    end
   end
+
+private
+
+VALID_EMAIL_REGEX =
+  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  def is_a_valid_email?(email)
+    email =~ VALID_EMAIL_REGEX
+  end
+
 end
