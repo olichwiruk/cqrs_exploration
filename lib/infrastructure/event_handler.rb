ab2d17c872c15b27c604d1703bdb91ca346b4474
Marcin <olichwiruk@gmail.com>
2017-08-17 11:55:03 +0200

    implement on(event) method in event_handler module

--- lib/infrastructure/event_handler.rb
+++ lib/infrastructure/event_handler.rb
@@ -1,3 +1,12 @@
 module EventHandler
 
+  def on(*events, &block)
+    events.each do |event_name|
+      super(event_name) do |event|
+        event.data = event.data.to_hash.symbolize_keys
+        block.call(event)
+      end
+    end
+  end
+
 end
