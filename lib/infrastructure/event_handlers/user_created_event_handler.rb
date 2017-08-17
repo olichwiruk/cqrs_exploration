faae802d041725a8bda11853e7c54236218b8e09
Marcin <olichwiruk@gmail.com>
2017-08-17 14:31:09 +0200

    handle user_created_event

--- lib/infrastructure/event_handlers/user_created_event_handler.rb
+++ lib/infrastructure/event_handlers/user_created_event_handler.rb
@@ -1,4 +1,10 @@
-class UserCreatedEventHandler
-  include EventHandler
+require 'infrastructure/read_models/users_read_model'
 
+class UserCreatedEventHandler
+
+  def user_created(event)
+    UsersReadModel.add_user(event.aggregate_uid,
+                            User.new(name: event[:data]['name'],
+                                     email: event[:data]['email']))
+  end
 end
