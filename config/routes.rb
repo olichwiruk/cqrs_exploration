12338440d48d4f3d5c3982f0a89af260cc1091a1
Marcin <olichwiruk@gmail.com>
2017-08-25 12:23:16 +0200

    add login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   post '/users/new', to: 'users#create', as: 'create_user'
+  get '/login', to: 'users#log', as: 'login'
+  post '/login', to: 'users#login', as: 'login_user'
+  get '/logout', to: 'users#logout', as: 'logout'
 end
