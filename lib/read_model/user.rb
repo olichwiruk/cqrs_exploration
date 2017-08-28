5626ce8da37e89a49ea94130a1ca67bd2ad632b8
Marcin <olichwiruk@gmail.com>
2017-08-28 12:16:42 +0200

    add update_user method

--- lib/read_model/user.rb
+++ lib/read_model/user.rb
@@ -2,11 +2,15 @@
 
 require 'domain/domain/entity'
 require 'domain/events/user_created_event'
+require 'domain/events/user_updated_event'
 require 'securerandom'
 
 module ReadModel
   class User < Disposable::Twin
+    feature Changed
+
     property :id
+    property :uid
     property :name
     property :email
 
@@ -23,9 +27,21 @@ module ReadModel
         self
       end
 
+      def update_user(user_params)
+        uid = user_params['uid']
+        name = user_params['name']
+        email = user_params['email']
+        apply_event(UserUpdatedEvent.new(aggregate_uid: uid,
+                                         name: name,
+                                         email: email))
+        self
+      end
+
       def on_user_created(event)
         @uid = event.aggregate_uid
       end
+
+      def on_user_updated(event); end
     end
   end
 end
