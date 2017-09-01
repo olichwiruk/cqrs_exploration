b754dfaeab89c7e23409658a0e63c3f0b5d82d8f
Marcin <olichwiruk@gmail.com>
2017-09-01 16:54:08 +0200

    add order created event and event handler

--- lib/infrastructure/repositories/orders_repository.rb
+++ lib/infrastructure/repositories/orders_repository.rb
@@ -4,6 +4,16 @@ module Infrastructure
   module Repositories
     class OrdersRepository
       class << self
+        def add_order(uid, order)
+          user_uid = order['user_uid']
+
+          sql = <<-SQL
+            insert into orders (uid, user_uid, created_at, updated_at)
+            values ('#{uid}', '#{user_uid}', '#{Time.now}', '#{Time.now}')
+          SQL
+
+          ActiveRecord::Base.connection.execute(sql)
+        end
       end
     end
   end
--- lib/order/domain/order.rb
+++ lib/order/domain/order.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+
 module Order
   module Domain
     class Order
       class << self
         include Infrastructure::Entity
 
-        def create_new_order(params); end
+        def create_new_order(user_uid:, discount: 0)
+          apply_event(
+            Order::Events::OrderCreatedEvent.new(
+              aggregate_uid: SecureRandom.uuid,
+              user_uid: user_uid,
+              discount: discount
+            )
+          )
+        end
+
+        def on_order_created(event); end
       end
     end
   end
