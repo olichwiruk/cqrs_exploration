4d88e50a442bf416ccbbc21197c5a6f5c8a3f95a
Marcin <olichwiruk@gmail.com>
2017-09-15 13:02:05 +0200

    add new product form

---
+++ app/view_models/products/add_product_view_model.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Products
+  class AddProductViewModel
+    attr_reader :product, :csrf_token, :errors
+
+    def initialize(product:, csrf_token:, errors: {})
+      @product = product
+      @csrf_token = csrf_token
+      @errors = errors
+    end
+
+    def update(options)
+      self.class.new(options)
+    end
+  end
+end
