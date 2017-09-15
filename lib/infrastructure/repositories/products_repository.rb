fe95c8115a06086b8bd5b156a86746799425e838
Marcin <olichwiruk@gmail.com>
2017-09-15 11:49:02 +0200

    add products repository

---
+++ lib/infrastructure/repositories/products_repository.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Infrastructure
+  module Repositories
+    class ProductsRepository
+      class << self
+        # write
+        def save(product)
+          Product::Domain::Product.new(
+            AR::Product.create!(product.attributes)
+          )
+        end
+
+        # read
+        def all_products
+          AR::Product.all
+        end
+
+        def find(id)
+          Product::Domain::Product.new(
+            AR::Product.find(id)
+          )
+        end
+
+        def build(params)
+          Product::Domain::Product.new(
+            AR::Product.new(params)
+          )
+        end
+      end
+    end
+  end
+end
