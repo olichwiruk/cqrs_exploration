a925377b7a41c8aac1e322d9ed96ef58d00a41cd
Marcin <olichwiruk@gmail.com>
2017-09-15 12:09:39 +0200

    wrap user view models with users module

---
+++ app/view_models/users/user_registration_view_model.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Users
+  class UserRegistrationViewModel
+    attr_reader :user, :csrf_token, :errors
+
+    def initialize(user:, csrf_token:, errors: {})
+      @user = user
+      @csrf_token = csrf_token
+      @errors = errors
+    end
+
+    def update(**options)
+      self.class.new(options)
+    end
+  end
+end
---
+++ app/view_models/users/users_list_view_model.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Users
+  class UsersListViewModel
+    attr_reader :users, :current_user_id
+
+    def initialize(users:, current_user_id:)
+      @users = users
+      @current_user_id = current_user_id
+    end
+  end
+end
