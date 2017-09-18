829cf4ecb1bb72bb9f8779298ade52d1affb1363
Marcin <olichwiruk@gmail.com>
2017-09-18 15:48:44 +0200

    send hash of chosen products to basket_controller

--- app/view_models/products/products_list_view_model.rb
+++ app/view_models/products/products_list_view_model.rb
@@ -2,11 +2,14 @@
 
 module Products
   class ProductsListViewModel
-    attr_reader :products, :current_user_id
+    attr_reader :products, :basket_hash, :current_user_id, :csrf_token, :errors
 
-    def initialize(products:, current_user_id:)
+    def initialize(products:, basket_hash: {}, current_user_id:, csrf_token:, errors: {})
       @products = products
+      @basket_hash = basket_hash
       @current_user_id = current_user_id
+      @csrf_token = csrf_token
+      @errors = errors
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :products
+  resources :basket, only: [:create, :index]
   get '/login', to: 'users#login_view', as: 'login'
   post '/login', to: 'users#login', as: 'login_user'
   get '/logout', to: 'users#logout', as: 'logout'
