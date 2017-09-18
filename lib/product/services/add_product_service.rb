9458859bb3862bc34ffac5af52a8b7f617faffd8
Marcin <olichwiruk@gmail.com>
2017-09-18 15:27:24 +0200

    move services to bounded contexts

---
+++ lib/product/services/add_product_service.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Product
+  module Services
+    class AddProductService
+      class << self
+        M = Dry::Monads
+        ProductsRepo = Infrastructure::Repositories::ProductsRepository
+
+        def call(params)
+          params[:quantity] = params[:quantity].to_i
+          params[:price] = params[:price].to_i
+          params_validation = validate(params)
+          return params_validation unless params_validation.success?
+
+          product = ProductsRepo.build(params)
+          product.create
+          ProductsRepo.save(product)
+
+          params_validation
+        end
+
+        # @api private
+        Validator = Dry::Validation.Form do
+          configure do
+            config.messages = :i18n
+          end
+
+          required(:name).filled(:str?)
+          required(:quantity).filled(:int?, gteq?: 0)
+          required(:price).filled(:int?, gt?: 0)
+        end
+
+        # @api private
+        def validate(params)
+          validator_result = Validator.call(params)
+          if validator_result.success?
+            M.Right(true)
+          else
+            M.Left(validator_result.errors)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/product/services/update_product_service.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Product
+  module Services
+    class UpdateProductService
+      class << self
+        M = Dry::Monads
+        ProductsRepo = Infrastructure::Repositories::ProductsRepository
+
+        def call(params)
+          params[:product][:quantity] = params[:product][:quantity].to_i
+          params[:product][:price] = params[:product][:price].to_i
+          params_validation = validate(params[:product])
+          return params_validation unless params_validation.success?
+
+          product = ProductsRepo.find(params[:id])
+          product.update(params[:product])
+          ProductsRepo.update(product)
+
+          params_validation
+        end
+
+        # @api private
+        Validator = Dry::Validation.Form do
+          configure do
+            config.messages = :i18n
+          end
+
+          required(:name).filled(:str?)
+          required(:quantity).filled(:int?, gteq?: 0)
+          required(:price).filled(:int?, gt?: 0)
+        end
+
+        # @api private
+        def validate(params)
+          validator_result = Validator.call(params)
+          if validator_result.success?
+            M.Right(true)
+          else
+            M.Left(validator_result.errors)
+          end
+        end
+      end
+    end
+  end
+end
