30f297dd68108867254dbb4898715d0441486e36
Marcin <olichwiruk@gmail.com>
2017-10-17 11:45:34 +0200

    fix making next orders

--- lib/order/event_denormalizers/order_created_event_denormalizer.rb
+++ lib/order/event_denormalizers/order_created_event_denormalizer.rb
@@ -3,7 +3,15 @@
 module Order
   module EventDenormalizers
     class OrderCreatedEventDenormalizer
-      def order_created(event); end
+      BasketsRepo = Infrastructure::RepositoriesRead::BasketsRepository
+      OrdersRepo = Infrastructure::Repositories::OrdersRepository
+
+      def order_created(event)
+        # TODO
+        return unless event.discount_id.nil?
+        order = OrdersRepo.find_by(uuid: event.aggregate_id)
+        BasketsRepo.save(order.id)
+      end
     end
   end
 end
